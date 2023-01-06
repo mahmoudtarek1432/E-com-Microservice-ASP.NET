4d832f6c7ae11f4568d7ddf8e0328b9ad8a24623
Mahmoud Tarek <mahmoudps2000@gmail.com>
2023-01-07 00:01:49 +0200

    Basket service intialized the shopping carts entities

---
+++ E-com-Microservice-ASP.NET/Services/Basket/Entites/ShoppingCart.cs
@@ -0,0 +1,29 @@
+﻿namespace Basket.API.Entites
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string username)
+        {
+            this.UserName = username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                Items.ForEach(i => totalprice += i.Price * i.Quantity);
+                return totalprice;
+            }
+        }
+    }
+}
