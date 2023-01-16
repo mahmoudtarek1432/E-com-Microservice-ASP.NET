bdc7987556e595f3b33d75a52ee6bb1fd219f3ce
Mahmoud Tarek <mahmoudps2000@gmail.com>
2023-01-16 20:10:19 +0200

    created the ordering application

---
+++ E-com-Microservice-ASP.NET/Services/Ordering/Ordering.Application/Contracts/Persistance/IAsyncRepo.cs
@@ -0,0 +1,31 @@
+﻿using Ordering.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Persistance
+{
+    public interface IAsyncRepository<T> where T: EntityBase
+    {
+        public Task<IReadOnlyList<T>> GetAllAsync();
+
+        public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null);
+
+        public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T,bool>> predicate = null,
+                                         Func<IQueryable<T>, IOrderedQueryable<T>> OrderedBy = null,
+                                         string IncludeString = null,
+                                         bool DisableTracking = true);
+        public Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate = null,
+                                         Func<IQueryable<T>, IOrderedQueryable<T>> OrderedBy = null,
+                                         Expression<List<Func<T, object>>> Include = null,
+                                         bool DisableTracking = true);
+
+        public Task<T> GetByIdAsync(int Id);
+        public Task<T> AddAsync(T entity);
+        public Task<T> UpdateAsync(T entity);
+        public Task<T> DeleteAsync(T entity);
+    }
+}
