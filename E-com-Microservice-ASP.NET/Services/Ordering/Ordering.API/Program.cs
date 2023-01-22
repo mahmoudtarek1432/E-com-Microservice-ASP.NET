06efd1504b8aa92b3ef58f50258ebf33da3b2a69
Mahmoud Tarek <mahmoudps2000@gmail.com>
2023-01-22 23:55:21 +0200

    connected order api with infrastructure

--- E-com-Microservice-ASP.NET/Services/Ordering/Ordering.API/Program.cs
+++ E-com-Microservice-ASP.NET/Services/Ordering/Ordering.API/Program.cs
@@ -1,9 +1,12 @@
 using Ordering.Application.Services;
+using Ordering.Infrastructure.Service;
+using Ordering.Infrastructure.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddApplicationServices();
+builder.Services.AddInfrastructureService(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ E-com-Microservice-ASP.NET/Services/Ordering/Ordering.Infrastructure/Service/AddInfrastructureService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Contracts.Infrastructure;
+using Ordering.Application.Contracts.Model;
+using Ordering.Application.Contracts.Persistance;
+using Ordering.Infrastructure.E_mail;
+using Ordering.Infrastructure.Persistence;
+using Ordering.Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Service
+{
+    public static class InfrastructureService
+    {
+        public static IServiceCollection AddInfrastructureService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(o =>
+            {
+                o.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString"));
+            });
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
