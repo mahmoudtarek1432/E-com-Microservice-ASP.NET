b61729451ed8facbe7d314531d42573eeb0e871d
Mahmoud Tarek <mahmoudps2000@gmail.com>
2023-01-22 23:31:27 +0200

    composed the email service

--- E-com-Microservice-ASP.NET/Services/Ordering/Ordering.Infrastructure/Email/EmailService.cs
+++ E-com-Microservice-ASP.NET/Services/Ordering/Ordering.Infrastructure/Email/EmailService.cs
@@ -3,9 +3,11 @@ using Microsoft.Extensions.Options;
 using Ordering.Application.Contracts.Infrastructure;
 using Ordering.Application.Contracts.Model;
 using SendGrid;
+using SendGrid.Helpers.Mail;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,17 +15,38 @@ namespace Ordering.Infrastructure.E_mail
 {
     public class EmailService : IEmailService
     {
-        private readonly IOptions<EmailSettings> _emailService;
+        private readonly EmailSettings _emailSettings;
+        private readonly ILogger<EmailService> _logger;
+
         public EmailService(IOptions<EmailSettings> EmailService, ILogger<EmailService> Logger) 
         { 
-            _emailService = EmailService?? throw new ArgumentNullException(nameof(EmailService));
-            Logger = Logger ?? throw new ArgumentNullException();
+            _emailSettings = EmailService.Value?? throw new ArgumentNullException(nameof(EmailService));
+            _logger = Logger ?? throw new ArgumentNullException();
 
         }
 
-        public Task<bool> SendEmailAsync(Email email)
+        public async Task<bool> SendEmailAsync(Email email)
         {
-            var client = new SendGridClient
+            var client = new SendGridClient(_emailSettings.ApiKey);
+
+            var subject = email.Subject?? string.Empty;
+            var to = new EmailAddress(email.To);
+            var body = email.Body;
+
+            var from = new EmailAddress(_emailSettings.FromAddress, _emailSettings.FromName);
+
+            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, body, body);
+
+            var response = await client.SendEmailAsync(sendGridMessage);
+
+            _logger.LogInformation("Email sent.");
+
+            if(response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return true;
+            }
+            _logger.LogError("Email sending failed.");
+            return false;
         }
     }
 }
