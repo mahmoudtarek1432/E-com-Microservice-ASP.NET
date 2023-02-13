bb93621b1b2439edd84a556061d506cbb358f2b9
Mahmoud Tarek <mahmoudps2000@gmail.com>
2023-02-13 19:08:29 +0200

    back again

---
+++ EventBus.Messages/Events/ItegrationBaseEvent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Messages.Events
+{
+    public class ItegrationBaseEvent
+    {
+        public ItegrationBaseEvent()
+        {
+            Id = new Guid();
+            CreationDate = DateTime.UtcNow;
+        }
+
+        public ItegrationBaseEvent(Guid Id, DateTime time)
+        {
+            Id = Id;
+            CreationDate = time;
+        }
+
+        public Guid Id { get; private set; }
+        public DateTime CreationDate { get; private set; }
+    }
+}
