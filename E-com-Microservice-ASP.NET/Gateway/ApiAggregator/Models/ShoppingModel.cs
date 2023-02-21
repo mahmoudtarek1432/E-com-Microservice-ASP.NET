aa58a41b84b3efc3e4ebe4b9e7f810e38d7406cc
Mahmoud Tarek <mahmoudps2000@gmail.com>
2023-02-21 23:40:46 +0200

    Setting up aggregator

---
+++ E-com-Microservice-ASP.NET/Gateway/ApiAggregator/Models/ShoppingModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiAggregator.Models
+{
+    public class ShoppingModel
+    {
+        public string UserName { get; set; }
+        public BasketModel BasketWithProducts { get; set; }
+        public IEnumerable<OrderResponseModel> Orders { get; set; }
+    }
+}
