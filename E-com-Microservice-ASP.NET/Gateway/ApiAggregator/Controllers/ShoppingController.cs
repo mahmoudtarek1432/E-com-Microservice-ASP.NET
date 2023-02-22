dcee7ad1df0c218fe8d1c43b0236a5b78a6056b0
Mahmoud Tarek <mahmoudps2000@gmail.com>
2023-02-22 22:47:18 +0200

    finished the shoping aggregator

---
+++ E-com-Microservice-ASP.NET/Gateway/ApiAggregator/Controllers/ShoppingController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiAggregator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+    }
+}
--- E-com-Microservice-ASP.NET/Gateway/ApiAggregator/Services/BasketService.cs
+++ E-com-Microservice-ASP.NET/Gateway/ApiAggregator/Services/BasketService.cs
@@ -1,6 +1,21 @@
-﻿namespace ApiAggregator.Servives
+﻿using ApiAggregator.Extentions;
+using ApiAggregator.Models;
+
+namespace ApiAggregator.Servives
 {
-    public class BasketService
+    public class BasketService : IBasketService
     {
+        private readonly HttpClient _client;
+
+        public BasketService(HttpClient client)
+        {
+            _client = client ?? throw new ArgumentNullException(nameof(client));
+        }
+
+        public async Task<BasketModel> GetBasket(string userName)
+        {
+            var response = await _client.GetAsync($"/api/v1/Basket/{userName}");
+            return await response.ReadContentAs<BasketModel>();
+        }
     }
 }
